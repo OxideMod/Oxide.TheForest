538b8904ed7d6c2dbb77d33c8b93879b10223322
Luke Spragg <luke@spragg.me>
2018-05-22 17:27:02 -0400

    Oops, name isn't being set in Covalence

--- src/TheForestPlayer.cs
+++ src/TheForestPlayer.cs
@@ -40,6 +40,7 @@ namespace Oxide.Game.TheForest
             cSteamId = SteamDSConfig.clientConnectionInfo[entity.source.ConnectionId];
             steamId = cSteamId.m_SteamID;
             Id = steamId.ToString();
+            Name = (entity.GetState<IPlayerState>().name?.Sanitize() ?? Name) ?? "Unnamed";
             stats = entity.GetComponentInChildren<PlayerStats>();
             this.entity = entity;
         }
