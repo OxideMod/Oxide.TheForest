78870082c7fb6e358e8ef6e651feb64eb7aa26e5
Luke Spragg <luke@spragg.me>
2020-04-12 14:43:53 -0400

    Exact match and connected handling with FindPlayers

--- src/TheForestPlayerManager.cs
+++ src/TheForestPlayerManager.cs
@@ -1,4 +1,4 @@
-extern alias References;
+﻿extern alias References;
 
 using Oxide.Core;
 using Oxide.Core.Libraries.Covalence;
@@ -126,13 +126,42 @@ namespace Oxide.Game.TheForest
         /// <returns></returns>
         public IEnumerable<IPlayer> FindPlayers(string partialNameOrId)
         {
-            foreach (TheForestPlayer player in allPlayers.Values)
+            List<IPlayer> foundPlayers = new List<IPlayer>();
+
+            foreach (TheForestPlayer player in connectedPlayers.Values)
             {
-                if (player.Name != null && player.Name.IndexOf(partialNameOrId, StringComparison.OrdinalIgnoreCase) >= 0 || player.Id == partialNameOrId)
+                if (player.Name.Equals(partialNameOrId, StringComparison.OrdinalIgnoreCase) || player.Id == partialNameOrId)
                 {
-                    yield return player;
+                    foundPlayers = new List<IPlayer> { player };
+                    break;
+                }
+
+                if (player.Name.IndexOf(partialNameOrId, StringComparison.OrdinalIgnoreCase) > 0)
+                {
+                    foundPlayers.Add(player);
                 }
             }
+
+            if (foundPlayers.Count() > 0)
+            {
+                return foundPlayers;
+            }
+
+            foreach (TheForestPlayer player in allPlayers.Values)
+            {
+                if (player.Name.Equals(partialNameOrId, StringComparison.OrdinalIgnoreCase) || player.Id == partialNameOrId)
+                {
+                    foundPlayers = new List<IPlayer> { player };
+                    break;
+                }
+
+                if (player.Name.IndexOf(partialNameOrId, StringComparison.OrdinalIgnoreCase) > 0)
+                {
+                    foundPlayers.Add(player);
+                }
+            }
+
+            return foundPlayers;
         }
 
         #endregion Player Finding
